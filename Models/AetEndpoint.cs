3858d482724c1c6acfe867a83e81e8a1811e5ca1
Brian OBrien <BrianOBrien@shaw.ca>
2025-12-31 14:29:22 -0700

    initial commit of xojo project DICOM-Sync

--- Models/AetEndpoint.cs
+++
@@ -1,8 +0,0 @@
-namespace DicomSync.Models;
-
-public class AetEndpoint
-{
-    public string Title { get; set; } = string.Empty;
-    public string Hostname { get; set; } = string.Empty;
-    public int Port { get; set; } = 104;
-}
